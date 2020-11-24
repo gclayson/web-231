bf2187e9e20d336dc42b5b38def33ebc3a33f02f
gclayson <gclaysonarts@gmail.com>
2020-11-24 12:19:21 -0800

    Assignment 2.2

--- week-2/Clayson-assignment2.1.js
+++ week-2/Clayson-assignment2.1.js
@@ -1,1 +1,25 @@
-let myName = myName().{}
+/*
+===================================
+Title : Clayson- assignment -2.2
+Author : George Clayson
+Date : 24 November 2020
+Modified By: George Clayson
+Description:Create a Function in JavaScript
+=====================================
+*/
+//start Program
+/*
+Expected Output:
+George
+*/
+//variable declaration
+myName.george ="George"
+function myName() {
+    return myName.george
+}
+//Output
+
+console.log(myName())
+//end program
+
+
