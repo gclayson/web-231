9737563fc0861d8d8dafe52f3b84705504229ce6
gclayson <gclaysonarts@gmail.com>
2020-11-29 13:55:42 -0800

    Assignment3.2

---
+++ week-3/Assignment3.2.js
@@ -0,0 +1,48 @@
+
+// Title: Assignment 3.2
+// Author: George Clayson
+// Date: 11/24/2020
+// Modified by : George Clayson
+// Description:Pattern Matching Functions
+
+// Start Program
+    //fuction
+    function widget(thing, thing2){
+        if (thing===thing2) {
+        //output
+        console.log(true)
+        return true;
+        } 
+        else {
+          //output  
+        console.log(false)
+        return false;
+        }
+}// widget("biko","biko")
+    //function
+    function logMismatch(miss,miss2){
+    if (miss===miss2){
+    console.log(true)
+      return true
+    }    else {
+    //output
+    console.log(false)
+    return false
+    }
+    }
+    //output
+    logMismatch("sick","sick2")
+    logMismatch("sick","sick2")
+    logMismatch("sick","sick2")
+    logMismatch("sick","sick2")
+    logMismatch("sick","sick2")
+    logMismatch("sick","sick2")
+    //end program
+
+
+
+
+
+
+
+
