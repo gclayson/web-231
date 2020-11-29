d34ea5351d4f34b5725ef61880ea6aff696b5463
gclayson <gclaysonarts@gmail.com>
2020-11-29 14:04:51 -0800

    assignment4.2 array

---
+++ week-4/Assignment4.2.js
@@ -0,0 +1,28 @@
+
+/*
+Expected output
+
+George Clayson
+Exercise 4.2
+11/26/2020
+
+pear
+grape
+banana
+peach
+orange
+*/
+// start program
+var fruits = ["pear", "grape","banana", "peach","orange"]
+
+// function
+function getFruit(arr) {
+    for (var k = 0; k< arr.length; k++) {
+        console.log(arr[k]);
+    }
+    
+}
+// output ffrom getFruit() function
+getFruit(fruits);
+
+// end program
