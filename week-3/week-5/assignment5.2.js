c87e97e664e8cf67f6e7bbcb01eed9e73de1c7c7
gclayson <gclaysonarts@gmail.com>
2020-12-01 09:23:35 -0800

    Assignment5.2

---
+++ week-3/week-5/assignment5.2.js
@@ -0,0 +1,20 @@
+/*
+Expected output
+
+George Clayson
+Exercise 5.2
+12/1/2020
+
+pear
+grape
+banana
+peach
+orange
+*/
+// start program
+var foods=["steak","Potatoes","Pizza","Sushi","Tacos",]
+ foods.forEach (function(food) {
+     console.log(food);
+ });
+
+ // end program
